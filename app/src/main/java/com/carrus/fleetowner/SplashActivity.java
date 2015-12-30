6b6e987979c6bceb9365dcbb49efcddaba23a6a3
Sunny Saini <sunny.saini@clicklabs.in>
2015-12-30 12:06:31 +0530

    bugs fixed

--- app/src/main/java/com/carrus/fleetowner/SplashActivity.java
+++ app/src/main/java/com/carrus/fleetowner/SplashActivity.java
@@ -3,6 +3,8 @@ package com.carrus.fleetowner;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
 
 import com.carrus.fleetowner.utils.SessionManager;
 
@@ -19,6 +21,24 @@ public class SplashActivity extends BaseActivity {
         setContentView(R.layout.activity_splash);
         //Force Crash
 //        int i=1/0;
+
+        int density= getResources().getDisplayMetrics().densityDpi;
+        switch(density)
+        {
+            case DisplayMetrics.DENSITY_LOW:
+                Toast.makeText(this, "LDPI", Toast.LENGTH_SHORT).show();
+                break;
+            case DisplayMetrics.DENSITY_MEDIUM:
+                Toast.makeText(this, "MDPI", Toast.LENGTH_SHORT).show();
+                break;
+            case DisplayMetrics.DENSITY_HIGH:
+                Toast.makeText(this, "HDPI", Toast.LENGTH_SHORT).show();
+                break;
+            case DisplayMetrics.DENSITY_XHIGH:
+                Toast.makeText(this, "XHDPI", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
 
         mSessionManager = new SessionManager(this);
         new Handler().postDelayed(new Runnable() {
